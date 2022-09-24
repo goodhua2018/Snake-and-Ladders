6b1323b32d5291334a53feb3e6af2808473a24b8
Hua <hualyu.2018@gmail.com>
2022-09-24 23:09:21 +0930

    two players

--- main.js
+++ main.js
@@ -3,30 +3,103 @@ let diceBtn = document.querySelector('.dice');
 let showNum = document.querySelector('.show-num');
 let showDice = document.querySelector('.show-dice');
 
-// class Player {
-//     constructor(name) {
-//         this.name = name;
-//         this.position = 1;
+var mapSection = document.querySelector('.map');
+for (let i = 9; i >= 0; i--) {
+    var row = document.createElement('section');
+    row.classList.add('row');
+    mapSection.appendChild(row);
 
-//     }
+    if (i % 2 === 0) {
+        for (let j = 1; j <= 10; j++) {
+            let smallGrid = document.createElement('div');
+            smallGrid.textContent = i * 10 + j;
+            smallGrid.classList.add('small-grid');
+            row.appendChild(smallGrid)
+        } 
+    } else {
+        for (let j = 10; j >= 1; j--) {
+        let smallGrid = document.createElement('div');
+        smallGrid.textContent = i * 10 + j;
+        smallGrid.classList.add('small-grid');
+        row.appendChild(smallGrid)
+        }
+    }
+}
 
-// }
-const player1 = {
-        name: 'Bunny',
-        imgUrl: './images/bunny.png',
-        position: 0
-    };
-const player2 = {
-        name: 'Cow',
-        imgUrl: './images/bunny.png',
-        position: 0
+
+class Player {
+    constructor(name) {
+        this.name = name;
+        this.imgUrl = '';
+        this.position = 0;
+        // bind this
+        this.diceBunny = this.diceBunny.bind(this);
+        this.diceCow = this.diceCow.bind(this);
+        this.flag = true;
+        
+        
+    }
+    diceBunny() {
+        let img1 = document.querySelector('.img1');
+        let randomNum = Math.floor(Math.random() * 6) + 1;
+        showNum.textContent = randomNum;
+    // var showDice = document.createElement('img');
+        showDice.src = "./images/dice" + randomNum + ".gif";
+        this.position += randomNum;
+        console.log(this)
+        console.log(this.position);
+  
+        img1.style.top = `-${Math.ceil(this.position / 10) * 50}px`;
+        if (Math.floor(this.position / 10) % 2 === 1) {
+            img1.style.left = `${(9 - this.position % 10) * 50}px`;
+
+        } else {
+            img1.style.left = `${this.position % 10 * 50}px`;
+        }
+        this.flag = false;
+       
 
     }
 
-var player1Div = document.querySelector('.player1')
-var img1 = document.querySelector('.img1');
-var player2Div = document.querySelector('.player2')
+    diceCow() {
+        let img2 = document.querySelector('.img2');
+        let randomNum = Math.floor(Math.random() * 6) + 1;
+        showNum.textContent = randomNum;
+    // var showDice = document.createElement('img');
+        showDice.src = "./images/dice" + randomNum + ".gif";
+        this.position += randomNum;
+        console.log(this)
+        console.log(this.position);
+  
+        img2.style.top = `-${Math.ceil(this.position / 10) * 50}px`;
+        if (Math.floor(this.position / 10) % 2 === 1) {
+            img2.style.left = `${(9 - this.position % 10) * 50}px`;
 
+        } else {
+            img2.style.left = `${this.position % 10 * 50}px`;
+        }
+
+
+    }
+}
+var bunny = new Player('bunny', 0)
+var cow = new Player('Cow', 0)
+
+var steps = 0;
+// if (steps % 2 === 0) {
+    
+// if (diceBtn.onclick = bunny.diceBunny;
+//     steps++;
+//     console.log(steps);
+// } 
+// if (steps % 2 === 1) {
+    
+//     diceBtn.onclick = cow.diceCow;
+//     steps++;
+//     console.log(steps);
+// }
+
+/*
 function dice() {
     randomNum = Math.floor(Math.random() * 6) + 1;
     showNum.textContent = randomNum;
@@ -41,42 +114,13 @@ function dice() {
 
     } else {
         img1.style.left = `${player1.position % 10 * 50}px`;
-    }
-    
-    
-
-    
-
-
+    } 
 }
+*/
 
 
-diceBtn.addEventListener('click', dice)
 
-var mapSection = document.querySelector('.map');
-for (let i = 9; i >= 0; i--) {
-    var row = document.createElement('section');
-    row.classList.add('row');
-    mapSection.appendChild(row);
-    
-    if (i % 2 === 0) {
-        
-        for (let j = 1; j <= 10; j++) {
-            let smallGrid = document.createElement('div');
-            smallGrid.textContent = i * 10 + j;
-            smallGrid.classList.add('small-grid');
-            row.appendChild(smallGrid)
-        } 
-    } else {
-        
-        for (let j = 10; j >= 1; j--) {
-        let smallGrid = document.createElement('div');
-        smallGrid.textContent = i * 10 + j;
-        smallGrid.classList.add('small-grid');
-        row.appendChild(smallGrid)
-        }
-    }
-}
+
 
 
     
